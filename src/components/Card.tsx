462f5b1303586daaf136c36c71fa196771779c9d
levworkshop <noreply@githubusers.com>
2022-11-10 19:52:47 +0200

    card component with props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,34 @@
-function Card() {
+interface Props {
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    category: string;
+    rating: number;
+}
+
+function Card({
+    name,
+    price,
+    description,
+    image,
+    category,
+    rating }: Props) {
+
     return (
-        <div>
-            Pad Tai
+        <div className="card">
+            <img src={image} className="card-img-top" alt={name} />
+            <div className="badge text-bg-info">{category}</div>
+            <div className="card-body">
+                <h5 className="card-title"><a href="#">{name}</a></h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text">{price}</p>
+                <p className="card-text">
+                    Rating: {rating}
+                    <i className="bi-star"></i>
+                </p>
+                <a href="#" className="btn btn-primary">Order Now</a>
+            </div>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,32 @@ function Home() {
         <>
             <Title />
 
-            <Card />
-            <Card />
-            <Card />
+            <Card
+                name="Greek Salad"
+                price={30.5}
+                description="Great salad..."
+                image="https://cdn.pixabay.com/photo/2016/08/09/10/30/tomatoes-1580273__340.jpg"
+                category="Vegeterian"
+                rating={4}
+            />
+
+            <Card
+                name="Pad Thai"
+                price={42}
+                description="Yumi yumi"
+                image="https://cdn.pixabay.com/photo/2017/02/25/15/23/pad-thai-2098017__340.jpg"
+                category="Asian"
+                rating={5}
+            />
+
+            <Card
+                name="Fried Chicken"
+                price={98.60}
+                description="I like it"
+                image="https://cdn.pixabay.com/photo/2014/01/16/01/48/chicken-nuggets-246180__340.jpg"
+                category="Chicken"
+                rating={3}
+            />
         </>
     );
 }
