940313c61fea9aed4cc98af27713b3229258f47a
levworkshop <noreply@githubusers.com>
2023-06-05 18:35:33 +0300

    add new dish

--- server/controllers/dishes.js
+++ server/controllers/dishes.js
@@ -1,4 +1,5 @@
 const database = require('./database');
+const joi = require('joi');
 
 module.exports = {
     getAll: async function (req, res, next) {
@@ -14,6 +15,33 @@ module.exports = {
     },
 
     addNew: async function (req, res, next) {
+        const schema = joi.object({
+            name: joi.string().required().min(2).max(100),
+            description: joi.string().required().min(2).max(300),
+            price: joi.number().required(),
+            rating: joi.number().required(),
+            image: joi.string().required(),
+            categoryId: joi.number().required(),
+        })
 
+        const { error, value } = schema.validate(req.body);
+
+        if (error) {
+            console.error(error.details[0].message);
+            res.status(400).json({ error: "invalid data" });
+            return;
+        }
+
+        const sql = "INSERT INTO dishes(name, description, price, rating, image, categoryId)" +
+            " VALUES(?,?,?,?,?,?);";
+
+        try {
+            const result = await database.query(sql, Object.values(value));
+            res.json({ ...value, id: result.id });
+        }
+        catch (err) {
+            console.error(err);
+            res.status(400).json({ error: err });
+        }
     }
 }
