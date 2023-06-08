46fb34b3e288e79bc28cf6b0e097296cca978a9c
levworkshop <noreply@githubusers.com>
2023-06-08 21:01:03 +0300

    style header with MUI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,56 @@
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand">
-                    <i className="bi-cup-hot me-2"></i>
-                    Meal On Time
-                </NavLink>
+        // <nav className="navbar navbar-dark bg-dark">
+        //     <div className="container-fluid">
+        //         <NavLink to="/" className="navbar-brand">
+        //             <i className="bi-cup-hot me-2"></i>
+        //             Meal On Time
+        //         </NavLink>
 
-                <ul className="navbar-nav flex-row">
-                    <li className="nav-item me-3">
+        //         <ul className="navbar-nav flex-row">
+        //             <li className="nav-item me-3">
+        //                 <NavLink to="/add-dish" className="nav-link">
+        //                     Add Dish
+        //                 </NavLink>
+        //             </li>
+        //             <li className="nav-item me-3">
+        //                 <NavLink to="/about" className="nav-link">
+        //                     About
+        //                 </NavLink>
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </nav>
+
+        <AppBar
+            position="static"
+            color="default"
+            enableColorOnDark
+        >
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Button color="warning">
+                        <NavLink to="/" className="navbar-brand">
+                            <i className="bi-cup-hot me-2"></i>
+                            Meal On Time
+                        </NavLink>
+                    </Button>
+                    <Button color="warning">
                         <NavLink to="/add-dish" className="nav-link">
                             Add Dish
                         </NavLink>
-                    </li>
-                    <li className="nav-item me-3">
+                    </Button>
+                    <Button color="warning">
                         <NavLink to="/about" className="nav-link">
                             About
                         </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+                    </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
